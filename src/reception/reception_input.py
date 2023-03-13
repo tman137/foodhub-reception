954a199375830ed964a9ed4f7530b2207ba67edd
Marco Pfatschbacher <marco@graylog.com>
2023-03-13 15:52:01 +0100

    Replace hot event loop with event.wait()
    I've noticed the program was constantly using 100% CPU,
    so I've optimized the event loop a bit.

--- src/reception/reception_input.py
+++ src/reception/reception_input.py
@@ -7,11 +7,11 @@ class ReceptionInput:
 
     def poll_qr_code(self):
         result = None
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    result = self.code
-                    self.code = ""
-                else:
-                    self.code += event.unicode
+        event = pygame.event.wait(1000)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                result = self.code
+                self.code = ""
+            else:
+                self.code += event.unicode
         return result
